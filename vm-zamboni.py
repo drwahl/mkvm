3284c48f06e016ca3e4aa95f7c78337f61a3652b
David Wahlstrom <dwahlstrom@classmates.com>
2010-11-19 13:44:25 -0800

    building out vm-zamboni.py. starting to get dangerous (aka, will delete stuff).

--- vm-zamboni.py
+++ vm-zamboni.py
@@ -2,7 +2,28 @@
 
 import time
 import xmlrpclib
+import logging
+import sys
 #import mkvm
+
+global_log_level = logging.WARN
+default_log_file = '/var/log/mkvm/vm-zamboni.log'
+default_activity_log_file = '/var/log/mkvm/activity.log'
+default_log_format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+logging.basicConfig(filename=default_log_file,
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%y.%m.%d %H:%M:%S'
+                   )
+console = logging.StreamHandler(sys.stderr)
+console.setLevel(logging.WARN)
+formatter = logging.Formatter('%(name)s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger("v-zambonim").addHandler(console)
+
+log = logging.getLogger("vm-zamboni")
+log.debug("Starting log")
 
 while True:
     xenapi = xmlrpclib.Server('https://xenserver01.corp.sea1.cmates.com')
@@ -15,64 +36,57 @@ while True:
         if not all_vm_records[vm_uuid]['is_control_domain']:
             if 'expiry' in all_vm_records[vm_uuid]['other_config']:
                 if int(all_vm_records[vm_uuid]['other_config']['expiry']) < date:
-                    print "%s deleted" % all_vm_records[vm_uuid]['name_label']
-                    #for existing_vm in myvm.is_existing_vm():
-                    #    vbd, vif, existing_VDIs = [], [], []
-                    #    existing_VBDs = xencache._get_all_vm_records()[existing_vm]['VBDs']
-                    #    existing_VIFs = xencache._get_all_vm_records()[existing_vm]['VIFs']
-                    #log.info('sending power off command to VM %s' % existing_vm) 
-                    #try:
-                    #    log.debug("powering off %s" % existing_vm)
-                    #    xenapi.VM.hard_shutdown(xensession, existing_vm)
-                    #except:
-                    #    log.debug("power off command failed. Assuming VM is already shutdown...")
-                    #pass
-                    #
-                    #for uuid in existing_VBDs:
-                    #    existing_VDIs.append(xenapi.VBD.get_record(xensession, uuid)['Value']['VDI'])
-                    #    log.info('sending destroy command for VBD %s' % uuid)
-                    #    try:
-                    #        log.debug("destroying VBD %s" % uuid)
-                    #        xenapi.VBD.destroy(xensession, uuid)
-                    #    except:
-                    #        log.debug("VBD destroy command failed. Assuming VBD is already destroyed...")
-                    #        pass
-                    #
-                    #for uuid in existing_VIFs:
-                    #    log.info('sending destroy command for VIF %s' % uuid)
-                    #    try:
-                    #        log.debug("destroying VIF %s" % uuid)
-                    #        xenapi.VIF.destroy(xensession, uuid)
-                    #    except:
-                    #        log.debug("VIF destroy command failed. Assuming VIF is already destroyed...")
-                    #        pass
-                    #
-                    #for uuid in existing_VDIs:
-                    #    log.info('sending destroy command for VDI %s' % uuid)
-                    #    try:
-                    #        log.debug("destroying VDI %s" % uuid)
-                    #        xenapi.VDI.destroy(xensession, uuid)
-                    #    except:
-                    #        log.debug("VDI destroy command failed.  Assuming VDI is already destroyed...")
-                    #        pass
-                    #
-                    #log.info('sending destroy command for VM %s' % existing_vm)
-                    #try:
-                    #    log.debug("destroying VM %s" % existing_vm)
-                    #    xenapi.VM.destroy(xensession, existing_vm)
-                    #except:
-                    #    log.debug("VM destroy command failed.  Assuming VM is already destroyed...")
-                    #    pass
-                    #
-                    #if options.add_to_cobbler:
-                    #    log.info("removing cobbler profile for %s" % myvm.fqdn)
-                    #    cobbler.purge(myvm)
-                    #    log.info("VM (%s) was destroyed and its system profile (%s) was removed from cobbler" % (myvm.name, myvm.fqdn))
-                    #    else:
-                    #        log.info("VM (%s) was destroyed" % myvm.name)
-
-                else:
-                    print "%s saved" % all_vm_records[vm_uuid]['name_label']
+                    print "deleting %s" % all_vm_records[vm_uuid]['name_label']
+		    raw_input()
+		    vmcache = xenapi.VM.get_record(xensession, vm_uuid)['Value']
+                    vbd, vif, VDIs = [], [], []
+                    VBDs = vmcache['VBDs']
+                    VIFs = vmcache['VIFs']
+                    log.info('sending power off command to VM %s' % vm_uuid) 
+                    try:
+                        log.debug("powering off %s" % vm_uuid)
+                        xenapi.VM.hard_shutdown(xensession, vm_uuid)
+                    except:
+                        log.debug("power off command failed. Assuming VM is already shutdown...")
+                    pass
+                    
+                    for uuid in VBDs:
+                        VDIs.append(xenapi.VBD.get_record(xensession, uuid)['Value']['VDI'])
+                        log.info('sending destroy command for VBD %s' % uuid)
+                        try:
+                            log.debug("destroying VBD %s" % uuid)
+                            xenapi.VBD.destroy(xensession, uuid)
+                        except:
+                            log.debug("VBD destroy command failed. Assuming VBD is already destroyed...")
+                            pass
+                    
+                    for uuid in VIFs:
+                        log.info('sending destroy command for VIF %s' % uuid)
+                        try:
+                            log.debug("destroying VIF %s" % uuid)
+                            xenapi.VIF.destroy(xensession, uuid)
+                        except:
+                            log.debug("VIF destroy command failed. Assuming VIF is already destroyed...")
+                            pass
+                    
+                    for uuid in VDIs:
+                        log.info('sending destroy command for VDI %s' % uuid)
+                        try:
+                            log.debug("destroying VDI %s" % uuid)
+                            xenapi.VDI.destroy(xensession, uuid)
+                        except:
+                            log.debug("VDI destroy command failed.  Assuming VDI is already destroyed...")
+                            pass
+                    
+                    log.info('sending destroy command for VM %s' % vm_uuid)
+                    try:
+                        log.debug("destroying VM %s" % vm_uuid)
+                        xenapi.VM.destroy(xensession, vm_uuid)
+                    except:
+                        log.debug("VM destroy command failed.  Assuming VM is already destroyed...")
+                        pass
+                    
+                    log.info("VM (%s) was destroyed" % vmcache['name_label'])
 
     xenapi.session.logout(xensession)
 
